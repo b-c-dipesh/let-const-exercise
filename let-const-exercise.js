5ef13e775cc7cf095e1c5f13febb7e34f28be785
bc-dipesh <bcdipeshwork@gmail.com>
2023-02-05 18:06:58 -0500

    Complete let-const-exercise

---
+++ let-const-exercise.js
@@ -0,0 +1,24 @@
+// var PI = 3.14
+// var PI  = 3.14;
+// PI = 42; stop me from doing this!
+
+const PI = 3.14;
+
+// Quiz
+// What is the difference between var and let?
+// - var is global and function scoped whereas let is block scoped.
+// - variable hoisting is possible on var whereas let doesn't allow it.
+// - we can redeclare variable declared with var whereas let doesn't allow it.
+// - we can reassign value for a variable declared with var whereas let doesn't allow it.
+
+// What is the difference between var and const?
+// - var is global and function scoped whereas const is block scoped.
+// - variable hoisting is possible on var whereas const doesn't allow it.
+// - we can redeclare variable in var whereas let doesn't allow it.
+// - we can reassign value for a variable declared with var whereas const doesn't allow it.
+
+// what is the difference between let and const?
+// - we can reassign value for a variable declared with let whereas const doesn't allow it.
+
+// what is hoisting?
+// - It is the process of moving a variable or function to the top of the scope. This makes variable and functions declared with var be accessible before they are used later on the code.
